25f3493592e5ce4dcb9d0bba00ea3c3e5fc99276
emilio211204 <joseulcuango32@gmail.com>
2024-12-26 20:23:37 -0500

    añadidop

--- app-01/navigators/MainNavigator.tsx
+++ app-01/navigators/MainNavigator.tsx
@@ -10,6 +10,7 @@ import Pagina3Screen from '../screens/Pagina3Screen'
 import Pagina5Screen from '../screens/Pagina5Screen'
 import Pagina6Screen from '../screens/Pagina6Screen'
 import Pagina7Screen from '../screens/Pagina7Screen'
+import Pagina4Screen from '../screens/Pagina4Screen'
 
 
 const Stack = createStackNavigator()
@@ -33,6 +34,7 @@ function MyDrawer() {
             <Drawer.Screen name="Pagina1" component={Pagina1Screen} />
             <Drawer.Screen name="Pagina2" component={Pagina2Screen} />
             <Drawer.Screen name="Pagina3" component={Pagina3Screen} />
+            <Drawer.Screen name="Pagina4" component={Pagina4Screen} />
             <Drawer.Screen name="Pagina5" component={Pagina5Screen} />
             <Drawer.Screen name="Pagina6" component={Pagina6Screen} />
             <Drawer.Screen name="Pagina7" component={Pagina7Screen} />
